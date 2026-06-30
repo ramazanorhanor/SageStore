f786c83d806936cb0d5f70856abc55950f4caf95
ramazanorhanor <ramazanorhanor@hotmail.com>
2026-06-30 11:05:59 +0300

    programcs servis ve middleware notu eklendi

--- SageStore.Api/Program.cs
+++ SageStore.Api/Program.cs
@@ -1,12 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. burası dependency injection için gerekli olan servisleri eklediğimiz yerdir. Örneğin, veri tabanı bağlantısı, servisler, repositoryler gibi işlemler burada yapılır.
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. burası pipeline için gerekli olan middleware'leri eklediğimiz yerdir. Örneğin, hata sayfaları, yönlendirme, yetkilendirme gibi işlemler burada yapılır.
 
 app.UseAuthorization();
 
