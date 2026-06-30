193a2893e285ab8bf0a5bf9db1c67e78c0a6188e
ramazanorhanor <ramazanorhanor@hotmail.com>
2026-06-30 16:47:28 +0300

    30.06.2026 sali günü son commit

---
+++ SageStore.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SageStore.Application.Legacy.Abstract;
+using SageStore.Application.Legacy.Concrete;
+using SageStore.Domain.Entities;
+
+namespace SageStore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController(ICategoryService categoryService) : ControllerBase
+    {
+        public async Task<IActionResult> GetAsync()
+        {
+            var categories = categoryService.GetAllCategoriesAsync().Result;
+            return Ok(categories);
+        }
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] Category category)
+        {
+            var categories =await categoryService.AddAsync(category);
+            return Created($"api/categories/{categories.Id}",categories);
+        }
+    }
+}
--- SageStore.Api/Program.cs
+++ SageStore.Api/Program.cs
@@ -1,15 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using SageStore.Application.Legacy.Abstract;
+using SageStore.Application.Legacy.Concrete;
+using SageStore.Persistence.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container. burası dependency injection için gerekli olan servisleri eklediğimiz yerdir. Örneğin, veri tabanı bağlantısı, servisler, repositoryler gibi işlemler burada yapılır.
 
 builder.Services.AddControllers();
-
+builder.Services.AddScoped<ICategoryService, CategoryManager>();
+builder.Services.AddDbContext<StoreContext>(opt=> { 
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("StoreDb"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline. burası pipeline için gerekli olan middleware'leri eklediğimiz yerdir. Örneğin, hata sayfaları, yönlendirme, yetkilendirme gibi işlemler burada yapılır.
 
 app.UseAuthorization();
-
+app.MapPost("create-database", async (StoreContext context) =>
+{
+    await context.Database.EnsureCreatedAsync();
+});
 app.MapControllers();
 
 app.Run();
